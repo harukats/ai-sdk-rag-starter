7bf24b8a83ce3bcd696207149d2401b807952ad6
harukats <46627550+harukats@users.noreply.github.com>
2024-09-13 08:09:32 +0000

    Refactor Chat API route to include tool for adding resources to knowledge base

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,7 @@
+import { createResource } from '@/lib/actions/resources';
 import { openai } from '@ai-sdk/openai';
-import { convertToCoreMessages, streamText } from 'ai';
+import { convertToCoreMessages, streamText, tool } from 'ai';
+import { z } from 'zod';
 
 // Allow streaming response up to 30 seconds
 export const maxDuration = 30;
@@ -10,9 +12,21 @@ export async function POST(req: Request) {
   const result = await streamText({
     model: openai('gpt-4o'),
     system: `You are a helpful assistant. Check your knowledge base before answering any questions.
-Only respond to questions using information from tool calls.
-if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
     messages: convertToCoreMessages(messages),
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        parameters: z.object({
+          content: z
+            .string()
+            .describe('the content or resource to add to the knowledge base'),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+    },
   });
 
   return result.toDataStreamResponse();
