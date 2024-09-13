14fc410d89504245d5e9d83384a7d316dc54f262
harukats <46627550+harukats@users.noreply.github.com>
2024-09-13 06:30:42 +0000

    Create resource with embedding

--- lib/actions/resources.ts
+++ lib/actions/resources.ts
@@ -1,25 +1,35 @@
-"use server";
+'use server';
 
 import {
   NewResourceParams,
   insertResourceSchema,
   resources,
-} from "@/lib/db/schema/resources";
-import { db } from "../db";
+} from '@/lib/db/schema/resources';
+import { db } from '../db';
+import { generateEmbeddings } from '../ai/embedding';
+import { embeddings as embeddingsTable } from '../db/schema/embeddings';
 
 export const createResource = async (input: NewResourceParams) => {
   try {
     const payload = insertResourceSchema.parse(input);
 
-    const contentWithoutLineBreaks = payload.content.replace("\n", " ");
+    const contentWithoutLineBreaks = payload.content.replace('\n', ' ');
     const [resource] = await db
       .insert(resources)
       .values({ content: contentWithoutLineBreaks })
       .returning();
 
-    return "Resource successfully created.";
+    const embeddings = await generateEmbeddings(contentWithoutLineBreaks);
+    await db.insert(embeddingsTable).values(
+      embeddings.map((embedding) => ({
+        resourceId: resource.id,
+        ...embedding,
+      })),
+    );
+
+    return 'Resource successfully created and embedded.';
   } catch (e) {
     if (e instanceof Error)
-      return e.message.length > 0 ? e.message : "Error, please try again.";
+      return e.message.length > 0 ? e.message : 'Error, please try again.';
   }
 };
